840ac7358f200761f21329dd6341507e382743fd
Nick McDonald <nick.mcdonald.94@gmail.com>
2022-03-29 10:54:37 -0700

    removing old copyright notices

--- render/__init__.py
+++ render/__init__.py
@@ -1,13 +1,1 @@
-########################################################
-#
-# Copyright (C) 2020-2021 Nick McDonald <nick@lazymorninggames.com>
-#
-# This file is part of CNDL.
-#
-# CNDL can not be copied and/or distributed without the express
-#
-# permission of Nick McDonald
-########################################################
-
-
 from .render import * # noqa
