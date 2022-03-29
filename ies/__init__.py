f707d655b285646907afcef54cff6592b407906f
Nick McDonald <nick.mcdonald.94@gmail.com>
2022-03-29 10:59:59 -0700

    removing old copyright notices

--- ies/__init__.py
+++ ies/__init__.py
@@ -1,14 +1,2 @@
-########################################################
-#
-# Copyright (C) 2020-2021 Nick McDonald <nick@lazymorninggames.com>
-#
-# This file is part of CNDL.
-#
-# CNDL can not be copied and/or distributed without the express
-#
-# permission of Nick McDonald
-########################################################
-
-
 from .ies import * # noqa
 from .iesutils import * # noqa
