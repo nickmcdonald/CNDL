4c339348f94d82c2d626b7cae496c1fea4eab5d9
Nick McDonald <nick.mcdonald.94@gmail.com>
2022-03-29 10:59:12 -0700

    removing old copyright notices

--- menu/presets.py
+++ menu/presets.py
@@ -1,14 +1,3 @@
-########################################################
-#
-# Copyright (C) 2020-2021 Nick McDonald <nick@lazymorninggames.com>
-#
-# This file is part of CNDL.
-#
-# CNDL can not be copied and/or distributed without the express
-#
-# permission of Nick McDonald
-########################################################
-
 from enum import Enum
 
 from qtpynodeeditor import FlowScene, PortType
